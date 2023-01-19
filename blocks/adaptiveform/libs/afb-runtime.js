c0152f6528db1e0e8a1e15155f34e19c3a362642
Vijay Kumar J <jalagari@adobe.com>
2023-01-19 14:47:47 +0530

    Adding license on AF-core and formater

--- blocks/adaptiveform/libs/afb-runtime.js
+++ blocks/adaptiveform/libs/afb-runtime.js
@@ -1,3 +1,21 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2022 Adobe
+* All Rights Reserved.
+*
+* NOTICE: All information contained herein is, and remains
+* the property of Adobe and its suppliers, if any. The intellectual
+* and technical concepts contained herein are proprietary to Adobe
+* and its suppliers and are protected by all applicable intellectual
+* property laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe.
+* Adobe permits you to use and modify this file solely in accordance with
+* the terms of the Adobe license agreement accompanying it.
+*************************************************************************/
 import { propertyChange, ExecuteRule, Initialize, CustomEvent, Submit, RemoveInstance, AddInstance, Reset, RemoveItem, AddItem, Click, Change, FormLoad, FieldChanged, ValidationComplete, Valid, Invalid } from './afb-events.js';
 import Formula from './json-formula.js';
 import { formatDate, formatNumber, getSkeleton } from './afb-formatters.js';
