79f6e9716cada8919d57c78a32f3d9c02cd445dd
Pareesh Gupta <pareesh@Pareeshs-MacBook-Pro.local>
2023-07-25 19:05:59 +0530

    fix: Add support for repeatable panel

---
+++ blocks/form/fieldset.js
@@ -0,0 +1,56 @@
+function updateItem(item, index) {
+
+}
+
+function update(fieldset) {
+    const items = [...fieldset.children].filter((item) => item.tagName === 'FIELDSET');
+    items.forEach((item, index) => {
+        // update item name and legend for each item based on index.
+    });
+}
+
+function createButton(fd) {
+    const button = document.createElement('button');
+    button.className = `${fd.Name}-${fd.Label} fieldset-${fd.Label}`;
+    button.type = 'button';
+    return button;
+}
+
+function createItem(fieldset, removable = true) {
+    const item = document.createElement('fieldset');
+    item.innerHTML = fieldset.elements['#template'].innerHTML;
+    if (removable) {
+        const button = createButton({ Label: 'Remove', Name: fieldset.name });
+        button.onclick = () => {
+            item.remove();
+            update(fieldset);
+        };
+        item.append(button);
+    }
+    return item;
+}
+
+function validateMin(fieldset) {
+    const min = parseInt(fieldset.getAttribute('min') || -1);
+    if (min !== -1) {
+        for (let i = 0; i < min; i++) {
+            fieldset.insertBefore(createItem(fieldset, false), fieldset.elements['#add']);
+        }
+        update(fieldset);
+    }
+}
+
+export default async function decorate(form) {
+    [...form.querySelectorAll('fieldset[data-repeatable=true]')].forEach((fieldset) => {
+        fieldset.elements['#template'] = document.createElement('div');
+        fieldset.elements['#add'] = fieldset.querySelector('button') || createAdd({ Label: 'Add', Name: fieldset.name });
+        fieldset.elements['#add'].remove(); // remove add button from template
+        fieldset.elements['#template'].innerHTML = fieldset.innerHTML;
+        fieldset.elements['#add'].onclick = () => {
+            fieldset.insertBefore(createItem(fieldset), fieldset.elements['#add']);
+            update(fieldset);
+        };
+        fieldset.replaceChildren(fieldset.elements['#add']);
+        validateMin(fieldset)
+    });
+}
