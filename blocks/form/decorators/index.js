6deb0cc1b74190e6f4b69f1f005797ecbf3b82d6
Vijay Kumar J <jalagari@adobe.com>
2023-09-01 13:51:19 +0530

    Adding Rule engine from Nedbank

--- blocks/form/decorators/index.js
+++ blocks/form/decorators/index.js
@@ -1,9 +1,11 @@
+import { applyRuleEngine } from '../rules/index.js';
 import { transformFileDOM, transformFileRequest } from './attachments.js';
 import { transformCaptchaDOM, transformCaptchaRequest } from './recaptcha.js';
 
 export const transformers = [
   transformFileDOM,
   transformCaptchaDOM,
+  applyRuleEngine,
 ];
 
 export const asyncTransformers = [
