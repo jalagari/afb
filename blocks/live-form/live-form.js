c105191f031a4efaf4b892122c588c47da6f2677
vdua <vdua@adobe.com>
2024-03-13 13:49:40 +0530

    adding a block to show live form example within the page

---
+++ blocks/live-form/live-form.js
@@ -0,0 +1,30 @@
+import { buildBlock, decorateBlock, loadBlock } from '../../scripts/lib-franklin.js';
+
+export function showForm() {
+
+}
+
+export default async function decorate(block) {
+  const anchor = block.querySelector('a');
+  if (anchor) {
+    const p = document.createElement('p');
+    p.innerText = 'See the form in ';
+    const btn = document.createElement('a');
+    btn.innerText = 'action';
+    btn.href = '#';
+    p.append(btn);
+    block.innerHTML = '';
+    block.append(p);
+    const form = buildBlock('form', [[anchor]]);
+    block.append(form);
+    form.style.display = 'none';
+    decorateBlock(form);
+    loadBlock(form);
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      form.querySelector('form [type="submit"]').disabled = true;
+      form.style.display = 'block';
+      p.style.display = 'none';
+    });
+  }
+}
